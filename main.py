d37c5119612c552431482b49a0ac3b0e270557ce
phannhuquyen <phannhuquyen88@gmail.com>
2025-04-28 20:06:27 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print('nhap a so nguyen:')
+a=int(input())
+b=int(input())
+c=int(input())
+def tich(a,b,c):
+    print("Tích 3 số nguyên là: ",a*b*c)
+def hieu(a,b,c):
+    print("Hiệu 3 số nguyên là: ",a-b-c)
+tich(a,b,c)
+hieu(a,b,c)
