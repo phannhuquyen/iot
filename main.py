9441686d1cc5324673cc2720a034be6dfa44e747
phannhuquyen <phannhuquyen88@gmail.com>
2025-04-28 20:25:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 print('nhap a so nguyen:')
-a=int(input())
-b=int(input())
-c=int(input())
+a=float(input())
+b=float(input())
+c=float(input())
 def tich(a,b,c):
-    print("Tích 3 số nguyên là: ",a*b*c)
+    print("Tích 3 số thực là: ",a*b*c)
 def hieu(a,b,c):
-    print("Hiệu 3 số nguyên là: ",a-b-c)
+    print("Hiệu 3 số thực là: ",a-b-c)
 def tong(a,b,c):
-    print("Tổng 3 số nguyên là: ",a+b+c)
+    print("Tổng 3 số thực là: ",a+b+c)
 tong(a,b,c)
 hieu(a,b,c)
 tich(a,b,c)
