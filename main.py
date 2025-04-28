74903b867f68e344e1c9d2d86972cc4b1c3ef5ef
phannhuquyen <phannhuquyen88@gmail.com>
2025-04-28 20:10:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ def tich(a,b,c):
     print("Tích 3 số nguyên là: ",a*b*c)
 def hieu(a,b,c):
     print("Hiệu 3 số nguyên là: ",a-b-c)
-tich(a,b,c)
+def tong(a,b,c):
+    print("Tổng 3 số nguyên là: ",a+b+c)
+tong(a,b,c)
 hieu(a,b,c)
+tich(a,b,c)
